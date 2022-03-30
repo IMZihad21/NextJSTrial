d36a5473a7f0ec4f13be3fb5af6d2b8203377d32
Mofajjal Rasul <mofajjal.techforing@gmail.com>
2022-03-30 17:05:48 +0600

    Switch to full dynamic
    Signed-off-by: Mofajjal Rasul <mofajjal.techforing@gmail.com>

--- pages/blog/[id]/edit.js
+++ pages/blog/[id]/edit.js
@@ -69,7 +69,7 @@ export default function EditBlog({ blogData }) {
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
     const res = await fetch('https://nextmongoose.vercel.app/api/blog');
     const blogs = await res.json()
     const blogData = blogs?.data.find((item) => item._id === params.id);
@@ -77,24 +77,6 @@ export async function getStaticProps({ params }) {
     return {
         props: {
             blogData
-        },
-        revalidate: 1
+        }
     }
-}
-
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// the path has not been generated.
-export async function getStaticPaths() {
-    const res = await fetch('https://nextmongoose.vercel.app/api/blog');
-    const blogs = await res.json()
-    // Get the paths we want to pre-render based on posts
-    const paths = blogs?.data?.map((blog) => ({
-        params: { id: blog._id },
-    }))
-
-    // We'll pre-render only these paths at build time.
-    // { fallback: blocking } will server-render pages
-    // on-demand if the path doesn't exist.
-    return { paths, fallback: 'blocking' }
 }
